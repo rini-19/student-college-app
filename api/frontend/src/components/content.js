e92662229b596370fa35cf3f6f6417c9f1369b1f
rini-19 <rinipandey1910@gmail.com>
2021-03-22 04:22:17 +0530

    deployed app

---
+++ api/frontend/src/components/content.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+import ChartsByState from './stateChart';
+import ChartByCourse from './courseChart';
+
+const styles = (theme) => ({
+  paper: {
+    maxWidth: 936,
+    margin: 'auto',
+    overflow: 'hidden',
+  },
+  searchBar: {
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  },
+  searchInput: {
+    fontSize: theme.typography.fontSize,
+  },
+  block: {
+    display: 'block',
+  },
+  addUser: {
+    marginRight: theme.spacing(1),
+  },
+  contentWrapper: {
+    margin: '40px 16px',
+  },
+});
+
+function Content(props) {
+  const { classes, route } = props;
+
+  return (
+    <Paper className={classes.paper}>
+      <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+            {route==='chartStates'? <ChartsByState /> : <ChartByCourse />}
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.contentWrapper}> 
+        
+      </div>
+    </Paper>
+  );
+}
+
+Content.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Content);
+
